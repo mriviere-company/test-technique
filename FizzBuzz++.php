011a52daecac06e358f31c207d1c4f0e7be2a21c
LeBorgne42 <rivierematthieupro@gmail.com>
2021-07-18 13:50:49 +0200

    Add Test1 FizzBuzz++

---
+++ FizzBuzz++.php
@@ -0,0 +1,29 @@
+<?php
+    if ($argv[1]) {
+        $int = intval($argv[1]);
+        $return = null;
+        if ($int) {
+            if ($int % 3 === 0 && $int != 3)
+                $return = 'FIZZ';
+            if ($int % 5 === 0 && $int != 5)
+                $return .= 'BUZZ';
+            $squareInt = round(sqrt($int));
+            $primary = true;
+            if ($squareInt > 0) {
+                while ($squareInt) {
+                    if ($int % $squareInt === 0 && $squareInt > 1)
+                        $primary = false;
+                    $squareInt--;
+                }
+                if ($primary)
+                    $return = 'FIZZBUZZ++';
+            }
+            if ($return)
+                echo $return;
+                file_put_contents('fizzbuzz', $return, FILE_APPEND);
+        } else {
+            echo 'You need to enter a digit parameter.';
+        }
+    } else {
+        echo 'You need to enter a parameter.';
+    }
